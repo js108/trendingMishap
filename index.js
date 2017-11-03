a6fe033374fd8c1615e270cb1d4c500c3022f7d1
Joseph Schoech <js@js-cpu.local>
2017-11-02 22:27:31 -0600

    bot path test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const tweetReader = require('./myModules/tweetReader');
 const trendComposer = require('./myModules/trendComposer');
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build/bot')));
 
 // Put all API endpoints under '/api'
 app.get('/trends', (req, res) => {
